9e8b6ad8d354429a4d81469d73526f385dec3ae6
Lele <scps960423@gmail.com>
2015-02-24 20:09:58 +0800

    1.修正layout之物件ID名稱 2.新增接收SERVER端IP之SOCKET監聽功能 3.廣播修正 4.下次需要修正當連續多次點自動搜尋時會報port被使用之問題(鎖定按鈕連點時間)

--- PhoneMyApplication/app/src/main/java/com/example/admin/phonemyapplication/PhoneMainActivity.java
+++ PhoneMyApplication/app/src/main/java/com/example/admin/phonemyapplication/PhoneMainActivity.java
@@ -14,31 +14,34 @@ import java.io.DataOutputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.ServerSocket;
 import java.net.Socket;
 import android.os.Handler;
 import android.os.HandlerThread;
-import java.util.logging.LogRecord;
 
 
-public class PhoneMainActivity extends Activity {
+public class PhoneMainActivity extends Activity implements Button.OnClickListener  {
 
-    private static Button butConnect;
+    private static Button btnConnect;
     private static Button btnAutoConnect;
+    private static EditText serverIP;
+
     private static String SERVER_IP;
     private static int SERVER_PORT;
     private static DataOutputStream dout = null;
 
     // thread1 - UI 主畫面
     private Handler mUIHandler=new Handler();
-    // thread2
+
+    // thread2 - 廣播本機IP
     private Handler mThreadHandler;
     private HandlerThread mThread;
 
-
-    // thread3
+    // thread3 - 接收serverIP
     private Handler mThreadReciverHandler;
     private HandlerThread mThreadReciver;
 
+    private boolean tThreadStop=false;
 
     // MRCode常數區段
     public final String MRCODE_TRUN_OFF = "MRCode_CC_02";
@@ -51,58 +54,18 @@ public class PhoneMainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone_main);
 
-        butConnect=(Button)findViewById(R.id.but1);
-        butConnect.setOnClickListener(ChkIP);
+        // 連線
+        btnConnect=(Button)findViewById(R.id.btnConnect);
+        btnConnect.setOnClickListener(this);
 
-        btnAutoConnect=(Button)findViewById(R.id.button);
-        btnAutoConnect.setOnClickListener(autoConnect);
+        // 廣播本機IP
+        btnAutoConnect=(Button)findViewById(R.id.btnAutoConnect);
+        btnAutoConnect.setOnClickListener(this);
 
-        //editInput=(EditText)findViewById(R.id.editText);
-
+        // 取得字串
+        serverIP = (EditText) findViewById(R.id.editText);
+        //EditText serverPORT = (EditText) findViewById(R.id.ServerPORT);
     }
-
-//    public void ChkIP(View v) {// IP手動設定
-//
-//        EditText serverIP = (EditText) findViewById(R.id.editText);
-//        //EditText serverPORT = (EditText) findViewById(R.id.ServerPORT);
-//
-//        int Error = 0;
-//        String IP4[] = serverIP.getText().toString().split("\\.");
-//
-//        if(serverIP.getText().toString().isEmpty()){
-//            //請輸入ip
-//            Error = 1;
-//        }else if(IP4.length!=4){
-//            //錯誤的ip格式
-//            Error = 2;
-//            //}else if(Integer.parseInt(serverPORT.getText().toString()) <= 0 || Integer.parseInt(serverPORT.getText().toString()) > 65535){
-//            //	//錯誤的port
-//            //	Error = 3;
-//        }else
-//            for (String IP : IP4)
-//                if(Integer.parseInt(IP) < 0 || Integer.parseInt(IP) > 255)
-//                    Error = 2;
-//
-//        String MES = null;
-//        switch(Error){
-//            case 0:
-//                MES = "設定成功";
-//
-//                break;
-//            case 1:
-//                MES = "請輸入IP";
-//                break;
-//            case 2:
-//                MES ="不正確的IP格式";
-//                break;
-//            //case 3:
-//                //MES = getString(R.string.port_error);
-//              //  break;
-//        }
-//        //Toast.makeText(getApplication(),MES, Toast.LENGTH_LONG).show(); // 列印異常資訊
-//        Toast.makeText(getApplication(),MES,Toast.LENGTH_LONG).show();
-//    }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -122,131 +85,195 @@ public class PhoneMainActivity extends Activity {
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
-
-    private Button.OnClickListener autoConnect=new Button.OnClickListener(){
-        @Override
-        public void onClick(View v) {
-            // 宣告handler
-            mThread=new HandlerThread("autoConnect");
-            mThread.start();
-            // 找到handler
-            mThreadHandler=new Handler(mThread.getLooper());
-            // 啟動輸出字串
-            mThreadHandler.post(autoConnectTask);
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnConnect:
+                mCheckIP();
+                break;
+            case R.id.btnAutoConnect:
+                mAutoConnection();
+                break;
         }
-    };
+    }
 
+    // 方法:判斷ip
+    private void mCheckIP(){
+        int Error = 0;
+        String IP4[] = serverIP.getText().toString().split("\\.");
+        if(serverIP.getText().toString().isEmpty()){
+            //請輸入ip
+            Error = 1;
+        }else if(IP4.length!=4){
+            //錯誤的ip格式
+            Error = 2;
+            /*
+                暫時去除檢查PORT功能,因為目前port都固定
+             */
+            //}else if(Integer.parseInt(serverPORT.getText().toString()) <= 0 ||
+            //        Integer.parseInt(serverPORT.getText().toString()) > 65535){
+            //    //錯誤的port
+            //    Error = 3;
+        }else
+            for (String IP : IP4)
+                if(Integer.parseInt(IP) < 0 || Integer.parseInt(IP) > 255)
+                    Error = 2;
+        String MES = null;
+        switch(Error){
+            case 0:
+                mConnectServer();
+                break;
+            case 1:
+                MES = "請輸入IP";
+                break;
+            case 2:
+                MES ="不正確的IP格式";
+                break;
+            case 3:
+                MES = "port錯誤";
+                break;
+        }
+        Toast.makeText(getApplication(),MES, Toast.LENGTH_LONG).show(); // 列印異常資訊
+        Toast.makeText(getApplication(),MES,Toast.LENGTH_LONG).show();
+    }
 
+    // 方法:建立連線
+    private void mConnectServer(){
+        SERVER_IP= serverIP.getText().toString();
+        SERVER_PORT = 3579;
 
-    // Thread工作
-    private Runnable autoConnectTask=new Runnable() {
-        @Override
-        public void run() {
-            // try-catch區段
-            try {
-                byte[] msg = new String("connection successfully!!!").getBytes();
-                InetAddress addr = InetAddress.getByName("255.255.255.255");
-                DatagramSocket client = new DatagramSocket();
-                DatagramPacket sendPack =
-                        new DatagramPacket(msg, msg.length, addr,8888);
-                client.send(sendPack);
-                Toast.makeText(getBaseContext(),"自動搜尋",Toast.LENGTH_SHORT).show();
+        // 如thread存在則移除它
+        if(! mThread.isInterrupted()) {
+            try{
+                mThread.interrupt();
             }catch(Exception e){
                 Toast.makeText(getBaseContext(),e.toString(),Toast.LENGTH_LONG).show();
             }
         }
-    };
 
+        // 建立與SERVER連線
+        mThread=new HandlerThread("writer");
+        mThread.start();
+        mThreadHandler=new Handler(mThread.getLooper());
+        mThreadHandler.post(tSocketClient);
+    }
 
-    private Button.OnClickListener ChkIP=new Button.OnClickListener(){
+    // 方法:自動搜尋
+    private void mAutoConnection(){
+        // 如thread存在則移除它
+        if(mThread!=null) mThread.interrupt();
+
+        // 宣告handler
+        mThread = new HandlerThread("autoConnect");
+        mThread.start();
+        mThreadHandler = new Handler(mThread.getLooper());
+        mThreadHandler.post(tSendBrocast);
+    }
+
+    // 執行緒:廣播本機IP
+    private Runnable tSendBrocast=new Runnable() {
         @Override
-        public void onClick(View v) {
-
-
-            EditText serverIP = (EditText) findViewById(R.id.editText);
-            //EditText serverPORT = (EditText) findViewById(R.id.ServerPORT);
-
-
-            int Error = 0;
-            String IP4[] = serverIP.getText().toString().split("\\.");
-
-
-            if(serverIP.getText().toString().isEmpty()){
-                //請輸入ip
-                Error = 1;
-            }else if(IP4.length!=4){
-                //錯誤的ip格式
-                Error = 2;
-                //}else if(Integer.parseInt(serverPORT.getText().toString()) <= 0 || Integer.parseInt(serverPORT.getText().toString()) > 65535){
-                //	//錯誤的port
-                //	Error = 3;
-            }else
-                for (String IP : IP4)
-                    if(Integer.parseInt(IP) < 0 || Integer.parseInt(IP) > 255)
-                        Error = 2;
-
-            //String MES = null;
-
-            switch(Error){
-                case 0:
-                    // MES = "設定成功";
-                    SERVER_IP= serverIP.getText().toString();
-                    SERVER_PORT = 3579;
-
-                    // 宣告handler
-                    mThread=new HandlerThread("writer");
-                    mThread.start();
-                    // 找到handler
-                    mThreadHandler=new Handler(mThread.getLooper());
-                    // 啟動輸出字串
-                    mThreadHandler.post(socketWrite);
-
-
-                    break;
-//                    case 1:
-//                        MES = "請輸入IP";
-//                        break;
-//                    case 2:
-//                        MES ="不正確的IP格式";
-//                        break;
-//                    //case 3:
-                //MES = getString(R.string.port_error);
-                //  break;
-            }
-            //Toast.makeText(getApplication(),MES, Toast.LENGTH_LONG).show(); // 列印異常資訊
-            // Toast.makeText(getApplication(),MES,Toast.LENGTH_LONG).show();
-        }
-
-
-
-    };
-
-    // Thread工作
-    private Runnable socketWrite=new Runnable() {
-        @Override
-            public void run() {
-            // try-catch區段
-
-            String in;
-            Socket s = null;
-            DataInputStream din = null;
-            DataOutputStream dout = null;
+        public void run() {
+            String myIp;
             try {
-                s = new Socket(SERVER_IP, SERVER_PORT);// 連接伺服器(IP依您電腦位址來修改)
-                dout = new DataOutputStream(s.getOutputStream());// 得到輸出串流
+                // 調用自訂之外部方法取得本地IP
+                myIp = utils.getIPAddress(getApplication());
+                byte[] msg = new String(myIp).getBytes();
+                InetAddress addr = InetAddress.getByName("255.255.255.255");
+                DatagramSocket client = new DatagramSocket();
+                DatagramPacket sendPack =
+                        new DatagramPacket(msg, msg.length, addr, 8899);
+                client.send(sendPack);
+                Toast.makeText(getBaseContext(), "自動搜尋", Toast.LENGTH_SHORT).show();
+
+                // 如thread存在則移除它
+                if(mThreadReciver!=null) mThreadReciver.interrupt();
+                // 啟動另一個thread接收SERVER IP
+                mThreadReciver = new HandlerThread("recver");
+                mThreadReciver.start();
+                mThreadReciverHandler = new Handler(mThreadReciver.getLooper());
+                mThreadReciverHandler.post(tReciveFromPc);
+
+            } catch (Exception e) {
+                Toast.makeText(getBaseContext(), e.toString(), Toast.LENGTH_LONG).show();
+            }
+        }
+    };
+
+    // 執行緒:把IP填入EDITTEXT並啟動連線
+    private Runnable tAutoConnectServer=new Runnable() {
+        @Override
+        public void run() {
+            // 把IP填入EDITTEXT
+            EditText serverIP = (EditText) findViewById(R.id.editText);
+            serverIP.setText(SERVER_IP);
+
+            Toast.makeText(getBaseContext(), " 把IP填入EDITTEXT", Toast.LENGTH_LONG).show();
+
+            // 呼叫連線方法
+            mConnectServer();
+        }
+    };
+
+    // 執行緒:接收serverIP
+    private Runnable tReciveFromPc=new Runnable() {
+        @Override
+        public void run() {
+            String in;
+            ServerSocket ss=null;
+            Socket cs=null;
+            DataInputStream din=null;
+            try {
+                //
+                ss = new ServerSocket(3578);
+                cs = ss.accept();
+                din = new DataInputStream(cs.getInputStream());// 得到輸出串流
+                in = din.readUTF();// 向伺服器發送訊息
+
+                if (in.isEmpty()) {
+                    Toast.makeText(getApplication(), "Empty", Toast.LENGTH_LONG).show();
+                } else {
+                    SERVER_IP = in;
+                    Toast.makeText(getApplication(), SERVER_IP, Toast.LENGTH_LONG).show();
+                    // 把IP填入EDITTEXT並啟動連線
+                    mUIHandler.post(tAutoConnectServer);
+                }
+
+            } catch (Exception e) {
+                Toast.makeText(getBaseContext(), e.toString(), Toast.LENGTH_LONG).show();
+            }finally {
+                try {
+                    if (cs!=null) cs.close();
+                    if (ss!=null) ss.close();
+                    if (din!=null) din.close();
+                } catch (Exception e) {
+                    Toast.makeText(getBaseContext(), e.toString(), Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    };
+
+    // 執行緒:作為客戶端連線
+    private Runnable tSocketClient=new Runnable() {
+        @Override
+        public void run() {
+            String in;
+            Socket cs = null;
+            DataInputStream din;
+            try {
+                cs = new Socket(SERVER_IP, SERVER_PORT);// 連接伺服器(IP依您電腦位址來修改)
+                dout = new DataOutputStream(cs.getOutputStream());// 得到輸出串流
                 dout.writeUTF(MRCODE_CONNECT);// 向伺服器發送訊息
 
-                din = new DataInputStream(s.getInputStream());// 得到輸出串流
+                din = new DataInputStream(cs.getInputStream());// 得到輸出串流
                 in =din.readUTF();// 向伺服器發送訊息
 
                 if(in.equalsIgnoreCase("Connected")) {
-                    Toast.makeText(getApplication(),"Connected!!!!!!!!!!!!!",Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplication(),"Connected",Toast.LENGTH_LONG).show();
                 }else{
-
                     Toast.makeText(getApplication(),in,Toast.LENGTH_LONG).show();
                 }
 
@@ -254,15 +281,12 @@ public class PhoneMainActivity extends Activity {
                 Toast.makeText(getApplication(), e.toString(), Toast.LENGTH_LONG).show(); // 列印異常資訊
             } finally {// 用finally語句塊確保動作執行
                 try {
-                    if (dout != null)  dout.close();// 關閉輸入串流
-                    if (s != null) 	s.close();// 關閉Socket連接
-
+                    if (dout != null) dout.close();// 關閉輸入串流
+                    if (cs != null) cs.close();// 關閉Socket連接
                 } catch (Exception e) {
                     Toast.makeText(getApplication(), e.toString(),Toast.LENGTH_LONG).show();
                 }
             }
         }
     };
-
-
 }
---
+++ PhoneMyApplication/app/src/main/java/com/example/admin/phonemyapplication/utils.java
@@ -0,0 +1,47 @@
+package com.example.admin.phonemyapplication;
+
+import android.content.Context;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.Enumeration;
+
+/**
+ * Created by Admin on 2015/2/24.
+ */
+public class utils {
+
+    public static String getMacAddress(Context context) {
+        WifiManager wifiMan = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        WifiInfo wifiInf = wifiMan.getConnectionInfo();
+        return wifiInf.getMacAddress();
+    }
+
+    public static String getIPAddress(Context context) {
+        WifiManager wifiMan = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        WifiInfo wifiInf = wifiMan.getConnectionInfo();
+        long ip = wifiInf.getIpAddress();
+        if( ip != 0 )
+            return String.format( "%d.%d.%d.%d",
+                    (ip & 0xff),
+                    (ip >> 8 & 0xff),
+                    (ip >> 16 & 0xff),
+                    (ip >> 24 & 0xff));
+        try {
+            for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();) {
+                NetworkInterface intf = en.nextElement();
+                for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements();) {
+                    InetAddress inetAddress = enumIpAddr.nextElement();
+                    if (!inetAddress.isLoopbackAddress()) {
+                        return inetAddress.getHostAddress().toString();
+                    }
+                }
+            }
+        } catch (Exception e) {
+        }
+        return "0.0.0.0";
+    }
+}
+
